2440b4bc670f7c891855d018cff42f41dc31e5ea
seovalencia <carlosfabuel@gmail.com>
2020-12-06 10:31:00 +0100

    Added suport for image size and text image

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # scrape-instagram
 Scrape instagram tags page to get the images and show them on a html page
+Instagram offer 5 different image sizes from 0 to 4.
 
+You can select the best resolution for your project.
 Also you can make a cron task to generate an html code on a daily basis to improve the content of your site or app. 
 
 This script uses python 2.7 to get the images from the json instagram code.
-You can see the final result on https://chulilla.com.
+You can see the final result on https://chulilla.com. or https://chulilla.net
 
 There, you can see the last images for the tag 'chulilla'
 
-You can modify the tag, server path and links to customize this code.
+You can modify the tag, server path, image size and links to customize this code.
+Also you can scrape the text added for each image. Be careful with the codification.
--- instaTag.py
+++ instaTag.py
@@ -5,11 +5,19 @@ import re
 import os
 import urllib
 import time
-
+#-------------
+#CONFIGURATION
+#-------------
 tag='chulilla'
-mypath = 'chulilla.com/insta/'
+mypath = '/home/carlos/testeo/insta/subir/scrape-instagram'
 myurl='https://chulilla.com/'
-
+folder='/images/'
+e_folder='insta/'
+image_size=4
+"""
+You can add the variableimage_text to the html and display it
+"""
+#-------------
 r = requests.get('https://www.instagram.com/explore/tags/'+tag+'/')
 soup = BeautifulSoup(r.text, 'html.parser')
 script = soup.find('script', text=lambda t: t.startswith('window._sharedData'))
@@ -20,16 +28,15 @@ contenido=''
 contador=0
 cHtml='<div class="flicontainer"><div class="fligrid">'
 for post in data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']['edges']:
-	image_src = post['node']['thumbnail_resources'][1]['src']
+	image_src = post['node']['thumbnail_resources'][image_size]['src']
+	image_text=post['node']['edge_media_to_caption']['edges'][0]['node']['text']
 	print(image_src)
-	#urllib.request.urlretrieve(image_src, os.path.basename(image_src))
-	#urllib.request.urlretrieve(image_src, my_path+"chulilla-"+str(contador)+".jpg")
-	urllib.urlretrieve(image_src, mypath+"/images/"+tag+str(contador)+".jpg")	
+	urllib.urlretrieve(image_src, mypath+folder+tag+str(contador)+".jpg")	
 	contenido=contenido+image_src+"\n"
-	cHtml=cHtml+'<div class="flimg"><img src="'+myurl+'/images/'+tag+str(contador)+'.jpg'+'" alt="" class="responsive-image"></div>'
+	cHtml=cHtml+'<div class="flimg"><img src="'+myurl+e_folder+tag+str(contador)+'.jpg'+'" alt="" class="responsive-image"></div>'
 	contador=contador+1
 	time.sleep(2)
 cHtml=cHtml+'</div></div>'
-f = open(mypath+tag+'.html','w+')
+f = open(mypath+folder+tag+'.html','w+')
 f.write(cHtml)
 f.close
